81ad32c18327a921713db5204217451a1cc85ad5
Ben Rowe <ben.rowe.83@gmail.com>
2016-06-15 08:08:01 +1000

    Add new WidgetFactory to extend over base

---
+++ src/Factories/WidgetFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Benrowe\Laravel\Widgets\Factories;
+
+use Arrilot\Widgets\Factories\WidgetFactory as BaseWidgetFactory;
+use Benrowe\Laravel\Widgets\Traits\BeginEndFactory;
+
+/**
+ *
+ *
+ * @package Benrowe\Laravel\Widgets
+ */
+class WidgetFactory extends BaseWidgetFactory
+{
+    use BeginEndFactory;
+}
