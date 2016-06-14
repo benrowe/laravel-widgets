4c8748f13a8f455d390bce2e90f311fdd9195ef0
Ben Rowe <ben.rowe.83@gmail.com>
2016-06-15 08:08:26 +1000

    Add trait for begin & end factory methods

---
+++ src/Traits/BeginEndFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Benrowe\Laravel\Widgets\Traits;
+
+/**
+ * Provides the begin & end functionality for the Widget factory
+ *
+ * @package Benrowe\Laravel\Widgets
+ */
+trait BeginEndFactory
+{
+    private $stack = [];
+
+    public function begin()
+    {
+
+    }
+
+    public function end()
+    {
+
+    }
+}
