37759cc43da8d84ac6d1d9031a23c9a30cfd06a0
Ben Rowe <ben.rowe.83@gmail.com>
2016-06-15 08:39:20 +1000

    Add method asExpression to WidgetFactory

--- src/Factories/WidgetFactory.php
+++ src/Factories/WidgetFactory.php
@@ -3,6 +3,7 @@
 namespace Benrowe\Laravel\Widgets\Factories;
 
 use Arrilot\Widgets\Factories\WidgetFactory as BaseWidgetFactory;
+use Benrowe\Laravel\Widgets\Expression;
 use Benrowe\Laravel\Widgets\Traits\BeginEndFactory;
 
 /**
@@ -13,4 +14,15 @@ use Benrowe\Laravel\Widgets\Traits\BeginEndFactory;
 class WidgetFactory extends BaseWidgetFactory
 {
     use BeginEndFactory;
+
+    /**
+     * Convert the outputted html to an expression
+     *
+     * @param  string $html
+     * @return Expression
+     */
+    protected function asExpression($html)
+    {
+        return new Expression($html, $this->widget);
+    }
 }
