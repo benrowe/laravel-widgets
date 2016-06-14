93b6a9106a45b908fb640fe2ec951ee727f3b5d2
Ben Rowe <ben.rowe.83@gmail.com>
2016-06-15 08:08:58 +1000

    Add placeholder trait for caching mechanism

---
+++ src/Traits/Cache.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Benrowe\Laravel\Widgets\Traits;
+
+/**
+ * Caching trait for Widgets
+ *
+ * By including this trait in with your widget, it will cache it's output
+ *
+ * @package Benrowe\Laravel\Widgets\Traits
+ */
+trait Cache
+{
+    /**
+     * The duration of the cache in seconds
+     *
+     * @return int
+     */
+    public function cacheDuration()
+    {
+        return  300;
+    }
+
+    /**
+     * [cacheEnabled description]
+     * @return [type] [description]
+     */
+    public function cacheEnabled(dfs)
+    {
+        return true;
+    }
+}
