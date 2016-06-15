d8405ad239364c81e54569aa3e835d34289f9063
Ben Rowe <ben.rowe.83@gmail.com>
2016-06-16 08:02:24 +1000

    Add widget get/set id to AbstractWidget

--- src/AbstractWidget.php
+++ src/AbstractWidget.php
@@ -3,12 +3,20 @@
 namespace Benrowe\Laravel\Widgets;
 
 use Arrilot\Widgets\AbstractWidget as BaseAbstractWidget;
+use Arrilot\Widgets\WidgetId;
 
 /**
  *
  */
 abstract class AbstractWidget extends BaseAbstractWidget
 {
+    const ID_PREFIX = 'widget-';
+
+    /**
+     * @var string
+     */
+    private $id;
+
     /**
      * Constructor
      * Store the configuration & initialise the widget
@@ -66,6 +74,31 @@ abstract class AbstractWidget extends BaseAbstractWidget
             return;
         }
         $this->config[$key] = $value;
+    }
+
+    /**
+     * Retrieve the widget id
+     *
+     * @param  boolean $autoGenerate automatically generate the id if none is
+     *                               already set
+     * @return string
+     */
+    public function getId($autoGenerate = true)
+    {
+        if (!$this->id && $autoGenerate) {
+            $this->id = self::ID_PREFIX . WidgetId::get();
+        }
+        return $this->id;
+    }
+
+    /**
+     * Set the widget identifier
+     *
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
     }
 
     /**
